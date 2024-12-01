d24e9d176f7ca68fdb48e5c7f37f1ff6c4e226ab
narendramongam <mongamnarendra@gmail.com>
2024-12-01 21:35:22 +0530

    second largest number in array

---
+++ Arrays/Easy/SecondLargestNumber.java
@@ -0,0 +1,48 @@
+package Arrays.Easy;
+
+import java.util.Arrays;
+
+public class SecondLargestNumber {
+
+    private static int getSecondMaxOptimized(int[] a)
+    {
+        int max=a[0];
+        for(int i:a)
+        {
+            if(i>max)
+            {
+                max=i;
+            }
+        }
+        int prev=-1;
+        for(int i:a)
+        {
+            if(i!=max && prev<i)
+            {
+                prev=i;
+            }
+        }
+        return prev;
+    }
+
+    private static int getSecondMaxBruteForce(int[] a)
+    {
+        Arrays.sort(a);
+        int secondMax=-1;
+        for(int i=a.length-2;i>=0;i--)
+        {
+            if(a[i]!=a[a.length-1])
+            {
+                secondMax=a[i];
+            }
+        }
+        return secondMax;
+    }
+    public static void main(String[] args) {
+        int[] a={5,10,10};
+        int maxElement= getSecondMaxOptimized(a);
+        int max=getSecondMaxBruteForce(a);
+        System.out.println(maxElement);
+        System.out.println(max);
+    }
+}
