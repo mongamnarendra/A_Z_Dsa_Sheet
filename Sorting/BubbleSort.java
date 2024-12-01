787f03d40b960504c61b571daed01b01374c27de
narendramongam <mongamnarendra@gmail.com>
2024-12-01 18:30:29 +0530

    BubbleSort is added

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,26 @@
 package Sorting;
 
-public class BubbleSort {
+import java.util.Arrays;
 
+public class BubbleSort {
+    public static void bubbleSort(int[] a)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a.length-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    int temp = a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] a={13,46,24,52,20,9};
+        bubbleSort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
